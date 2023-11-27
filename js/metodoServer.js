bae700876775f67bf688b848153c4f0e38fcec00
DiasoAndre <136815797+DiasoAndre@users.noreply.github.com>
2023-11-27 20:17:06 -0300

    Manutenção no saveUser

--- js/metodoServer.js
+++ js/metodoServer.js
@@ -1,19 +1,26 @@
 import { serverURL } from "./env.js"
 
-export async function saveUser(user)
-{
-    const response = await fetch(serverURL + '/users/register', {
-        method: 'POST',
-        body:{
-            name : user.name,
-            email : user.email,
-            email_reserva : user.email_reserva,
-            password : user.password
-        }
-    })
-    console.log(response)
-    if(response.status == 201)
-    {
-        return await response.json()
-    }
-}
+export async function saveUser(user) {
+    const url = serverURL + '/users/register';
+  
+    const data = {
+      name: user.name,
+      email: user.email,
+      email_reserva: user.email_reserva,
+      password: user.password,
+    };
+  
+    try {
+        const response = await fetch(url, {
+          method: 'POST',
+          body: JSON.stringify(data),
+          headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+          },
+        });
+        console.log(response);
+        return await response.json();
+      } catch (error) {
+        console.error('Erro na solicitação:', error);
+      }
+  }
