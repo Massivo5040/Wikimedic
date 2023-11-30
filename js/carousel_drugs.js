5cfe4bef4ced0740f1f120936b0a95f348b555ce
Arthur Rolemberg <arthurrolemberg4262@gmail.com>
2023-11-30 15:43:08 -0300

    Terminei o carousel (acho que ele parou de ser bipolar).

--- js/carousel_drugs.js
+++ js/carousel_drugs.js
@@ -1,18 +1,15 @@
 const carousel = document.querySelector(".carousel-container .carousel");
 const btns = document.querySelectorAll(".carousel-container span");
-const figure = carousel.querySelector("figure").clientWidth;
-const scroll = figure + (carousel.clientWidth / 100) * 5;
+const anchor = carousel.querySelector("a").clientWidth;
+const scroll = anchor + (carousel.clientWidth / 100) * 5;
 
 btns.forEach((btn) => {
   btn.addEventListener("click", () => {
-    console.log("entrou função");
     switch (btn.id) {
       case "left":
-        console.log("sequerda");
         carousel.scrollLeft += -scroll;
         break;
       case "right":
-        console.log("direita");
         carousel.scrollLeft += scroll;
         break;
     }
