2f83a38dc1ee0a84c0c3f171b43081fb13c4cfb9
DiasoAndre <136815797+DiasoAndre@users.noreply.github.com>
2023-11-30 13:16:01 -0300

    Vriáveis de ambiente adicionada

--- js/env.js
+++ js/env.js
@@ -1,7 +1,10 @@
 // variáveis de ambiente
-export const serverURL = "https://server-wikimedic.onrender.com/" //essa é a url do servidor
-export let userInfo = {
+export const serverURL = "https://server-wikimedic.onrender.com" //essa é a url do servidor
+export let userInfo = { // informações do usuário
     id : localStorage.getItem('idUser'),
+    name : localStorage.getItem('username'),
+    email_reserva : localStorage.getItem('email_reserva'),
     email : localStorage.getItem('email'),
-    password : localStorage.getItem('password')
+    password : localStorage.getItem('password'),
+    login : localStorage.getItem('login')
 }
