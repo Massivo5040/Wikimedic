98243bb9dc8a89226c17e3e82ddfd6fec36846ab
DiasoAndre <136815797+DiasoAndre@users.noreply.github.com>
2023-11-30 09:31:39 -0300

    added user info

--- js/env.js
+++ js/env.js
@@ -1,3 +1,7 @@
 // variáveis de ambiente
 export const serverURL = "http://localhost:8080" //essa é a url padrão do servidor, no meu ambiente de desenvolvimento
-export let userInfo = {}
+export let userInfo = {
+    id : localStorage.getItem('idUser'),
+    email : localStorage.getItem('email'),
+    password : localStorage.getItem('password')
+}
