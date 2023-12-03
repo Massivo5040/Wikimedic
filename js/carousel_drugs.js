b507f11a8d0266f629b0f3fb07a966d461ac1192
Arthur Rolemberg <arthurrolemberg4262@gmail.com>
2023-12-03 18:50:20 -0300

    Adiciona a página de signup, signin e esqueci minha senha.

--- js/carousel_drugs.js
+++ js/carousel_drugs.js
@@ -15,12 +15,3 @@ btns.forEach((btn) => {
     }
   });
 });
-
-const stars = document.querySelectorAll(".fa-star");
-
-stars.forEach((star) => {
-  star.addEventListener("click", (e) => {
-    star.classList.toggle("fa-solid");
-    console.log(e);
-  });
-});
